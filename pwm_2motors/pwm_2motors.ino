6034317c0fa77bf6feb18a657b9e066e22fb323e
parthiv-p <parthiv.parikh95@gmail.com>
2018-04-29 21:02:03 -0400

    Update motor control to 2 channels and add functions for direction control

--- pwm_2motors/pwm_2motors.ino
+++ pwm_2motors/pwm_2motors.ino
@@ -1,53 +1,57 @@
-/*
- *  20ms period
- *  pulse > 1.52ms: clockwise         ---> 2ms = 750 in C4V
- *  pulse < 1.48ms: counter clockwise ---> 1ms = 375 in C4V
- *  1.48ms < pulse < 1.52ms: stop
- * 
- */
 
-void setup() {
-Serial.begin(38400);
-delay(1000);
+void initPWM(){
+	FTM0_MODE |= (1U << 2) | (1U << 0); //Disable write protection
+	FTM0_SC |= (1U << 3) | (1U << 2);//use system clock, 16 prescalar
 
-PORTD_PCR4 |= (1U << 10); // mux to FTM0_C4 for pin 6
-PORTD_PCR5 |= (1U << 10); // mux to FTM0_C5 for pin 20
+	FTM0_C0SC |= (1U << 3) | (1U << 5); //edge aligned high output pc1
+	FTM0_C1SC |= (1U << 3) | (1U << 5); //edge aligned high output pc2
 
-/*
- * there are 3 FlexTimer modules.. FTM0, FTM1, FMT2... supports input capture, output compare, PWM
- *    - FTMx runs off of the BUS CLOCK
- *    - FTM time reference is a 16-bit counter... can be free running, or have stop and start, up or down
- *    - FTM0 has 8 channels. FTM0 has different usages than FTM1 and FTM2 (2 channels)
- *    - FTMs have multiple sources of interrupt, but they generate a single request...
- *        when an FTM interrupt occurs, read the FTM status register to determine the source
- *    - offers a prescaler divide-by 1, 2, 4, 8, 16, 32, 64, 128
- *
- */ 
+	FTMO_CNTIN = 0;
+	FTM0_MOD = 29999; //sets to 50hz period
 
-// use system clock... this sets all channels in FTM0
-FTM0_SC |= _BV(3); 
-FTM0_SC &= ~(_BV(4));
+	PORTC_PCR1 |= (1U << 10); //22 FTM0 ch0 alt 4
+	PORTC_PCR2 |= (1U << 10); //23 FTM0 ch1 alt 4
+}
 
+void forward(){
+	FTM0_C0V = 1500;
+	FTM0_C1V = 1500;
+}
 
-// disable write protection
-FTM0_MODE |= (1U << 2); //Mode register
-FTM0_MODE |= _BV(1); // Enables FTM, this must come after the line above since this bit is write protected.
+void reverse(){
+	FTM0_C0V = 3000;
+	FTM0_C1V = 3000;
+}
 
-FTM0_SC |= 0x7; // prescale by factor of 128
+void left(){
+	FTM0_C0V = 1500;
+	FTM0_C1V = 3000;
+}
 
-// this register is used to configure the interrupt enable, channel config, and pin function
-// set MSB and ELSB... sets edge aligned PWM mode, and high-true pulses (clear output on match)
-FTM0_C4SC |= (1U << 3) | (1U << 5); // channel 4
-FTM0_C5SC |= (1U << 3) | (1U << 5); // channel 5
+void right(){
+	FTM0_C0V = 3000;
+	FTM0_C1V = 1500;
+}
 
+void stop(){
+	FTM0_C0V = 2250;
+	FTM0_C1V = 2250;
+}
 
-FTM0_CNTIN = 0; // gives an initial value of 0 for the counter (used after reset)
-FTM0_C5V = 562; //900; // count up to C5V and clear output, C5V/MOD duty cycle %
-FTM0_C4V = 562; //900; // count up to C4V and clear output, C4V/MOD duty cycle %
-FTM0_MOD = 7500; //7500; //5625;// counts up to MOD... this is the period of cycle
+void setup(){
+	initPWM();	
 
 }
 
-void loop() {
-
+void loop(){
+	forward();
+	delay(2000);
+	stop();
+	delay(2000);
+	reverse();
+	delay(2000);
+	left();
+	delay(2000);
+	right();
+	delay(2000);
 }
