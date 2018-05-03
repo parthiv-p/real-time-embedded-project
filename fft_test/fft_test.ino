823ced355742f9f4f0a84091c1f1c3a66ecb3dce
Utku Unlu <h.utkuunlu@gmail.com>
2018-05-03 13:33:35 -0400

    Teensy audio lib FFT

--- fft_test/fft_test.ino
+++ fft_test/fft_test.ino
@@ -1,6 +1,6 @@
 #include "arduinoFFT.h"
  
-#define SAMPLES 128             //Must be a power of 2
+#define SAMPLES 512           //Must be a power of 2
 #define SAMPLING_FREQUENCY 22000 //Hz, must be less than 10000 due to ADC
  
 arduinoFFT FFT = arduinoFFT();
@@ -10,6 +10,8 @@ unsigned long microseconds;
  
 double vReal[SAMPLES];
 double vImag[SAMPLES];
+
+unsigned long Now;
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,7 +29,7 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 /*SAMPLING*/
-
+    Now = micros();
     for(int i=0; i<SAMPLES; i++)
     {
         microseconds = micros();    //Overflows after around 70 minutes!
@@ -46,18 +48,21 @@ void loop() {
     FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
     FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
     FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+
+    //Serial.println("Time: " + String(micros() - Now));
+    
     double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
  
     /*PRINT RESULTS*/ 
     for(int i=0; i<(SAMPLES/2); i++)
     {
         /*View all these three lines in serial terminal to see which frequencies has which amplitudes*/         
-        Serial.print((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
-        Serial.print(" ");
-        Serial.println(vReal[i], 1);    //View only this line in serial plotter to visualize the bins
+        Serial.println((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
+        //Serial.print(" ");
+        //Serial.println(vReal[i], 1);    //View only this line in serial plotter to visualize the bins
     }
 
-    Serial.println(peak);     //Print out what frequency is the most dominant.
+    //Serial.println(peak);     //Print out what frequency is the most dominant.
     delay(100);
     
 }
---
+++ teensy_audio_fft/teensy_audio_fft.ino
@@ -0,0 +1,67 @@
+// FFT Test
+//
+// Compute a 1024 point Fast Fourier Transform (spectrum analysis)
+// on audio connected to the Left Line-In pin.  By changing code,
+// a synthetic sine wave can be input instead.
+//
+// The first 40 (of 512) frequency analysis bins are printed to
+// the Arduino Serial Monitor.  Viewing the raw data can help you
+// understand how the FFT works and what results to expect when
+// using the data to control LEDs, motors, or other fun things!
+//
+// This example code is in the public domain.
+
+#include <Audio.h>
+
+const int myInput = AUDIO_INPUT_LINEIN;
+unsigned long Now, readTimer;
+
+AudioInputAnalog          audioInput;         // audio shield: mic or line-in
+//AudioAnalyzeFFT1024    myFFT;
+AudioAnalyzeToneDetect   tones[10];
+
+//AudioConnection patchCord1(audioInput, 0, myFFT, 0);
+AudioConnection patchCord2(audioInput, tones[0]);
+AudioConnection patchCord3(audioInput, tones[1]);
+AudioConnection patchCord4(audioInput, tones[2]);
+AudioConnection patchCord5(audioInput, tones[3]);
+AudioConnection patchCord6(audioInput, tones[4]);
+AudioConnection patchCord7(audioInput, tones[5]);
+AudioConnection patchCord8(audioInput, tones[6]);
+AudioConnection patchCord9(audioInput, tones[7]);
+AudioConnection patchCord10(audioInput, tones[8]);
+AudioConnection patchCord11(audioInput, tones[9]);
+
+void setup() {
+
+  AudioMemory(12);
+  for(int i = 0 ; i < 10 ; i++)
+  {
+    tones[i].frequency(5000+500*i, 50000);
+  }
+  //myFFT.windowFunction(AudioWindowHanning1024);
+}
+
+void loop() {
+  float mags[10] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+  int counter = 0;
+  Now = millis();
+  while(millis() - Now < 1250)
+  {
+    readTimer = micros();
+    for(int i = 0 ; i < 10 ; i++)
+    {
+      mags[i] += tones[i].read();
+    }
+    counter++;
+  }
+  Serial.print("Count: " + String(counter));
+  for(int i = 0 ; i < 10 ; i++)
+  {
+    Serial.print("\t" + String(mags[i]));
+  }
+  Serial.print("\n");
+
+}
+
+
